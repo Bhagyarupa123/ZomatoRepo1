b99f8773b7a570983096f162ef0a73e4d6803994
BhagyaKoppisetti <bkoppisetti@uworld.in>
2023-08-02 18:25:51 +0530

    Commit1

---
+++ CaseStudy/Entities/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CaseStudy.Entities
+{
+    public class Recipe
+    {
+        [Key]
+        
+        public int RecipeId { get; set; }
+        [Required]
+        public string RecipeName { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ CaseStudy/Entities/ZomatoDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CaseStudy.Entities
+{
+    public class ZomatoDBContext:DbContext
+    {
+        public ZomatoDBContext(DbContextOptions<ZomatoDBContext> options) : base(options) { }
+        public DbSet<Recipe>Recipes { get; set; }
+    }
+}
